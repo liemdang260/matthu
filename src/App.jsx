ac6d58d489e2423138ead9562001dbb4a2043b40
liemdang260 <69330905+liemdang260@users.noreply.github.com>
2022-07-09 21:31:21 +0700

    init header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Home from "./pages/Home";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Header/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+  return (
+    //
+    <div className="h-[50px] bg-teal-300 flex flex-row justify-center items-center relative">
+      <div className="inline-block w-[40px] h-[40px] absolute left-3 rounded-md overflow-hidden bg-white py-2 px-1 flex flex-col justify-around">
+        {Array.from(Array(3)).map((t) => (
+          <div className="h-[3px] bg-teal-300"></div>
+        ))}
+      </div>
+      {/* <div className="inline-block w-[40px] h-[40px]">Logo</div> */}
+      <div className="inline-block text-white text-xl font-semibold">
+        Mật Thư Online
+      </div>
+    </div>
+  );
+}
+
+export default Header;
